0b8bf8d9a72b910a5214bf42e252bfbbec58d60d
LEONARDO_DIAS <leonardodias2706@gmail.com>
2025-07-07 17:09:51 -0300

    grabcut evaluation

--- utils/metrics.py
+++ utils/metrics.py
@@ -15,3 +15,17 @@ def compute_iou(preds, masks, eps=1e-7):
     intersection = (preds_bin * masks).sum()
     union = preds_bin.sum() + masks.sum() - intersection
     return ((intersection + eps) / (union + eps)).item()
+
+
+def pixel_accuracy_evaluate(preds, masks):
+    """Calculate pixel accuracy (percentage of correct pixels) for evaluation"""
+    correct = (preds == masks).sum()
+    total = masks.shape[0] * masks.shape[1]
+    return correct / total
+
+
+def compute_iou_evaluate(preds, masks, eps=1e-7):
+    """Intersection over Union (IoU) for evaluation"""
+    intersection = (preds * masks).sum()
+    union = preds.sum() + masks.sum() - intersection
+    return (intersection + eps) / (union + eps)
